c1895342010891b3541f0cb48852654225e0c52f
Ben Tan <btan90@gmail.com>
2022-08-15 17:49:21 -0700

    update convert for mac

--- convert.py
+++ convert.py
@@ -9,10 +9,12 @@ for item in files_in_dir:
     if item.endswith(".jpg"):
         os.remove(item)
  
- # Convert to jpg
-poppler_path = r"C:\CS8398\pdf_to_image\poppler-22.04.0\Library\bin"  # update this path
+# Convert to jpg
+# poppler_path = r"C:\CS8398\pdf_to_image\poppler-22.04.0\Library\bin"  # Windows: update this path
+
 # Store Pdf with convert_from_path function
-images = convert_from_path(pdf_path = 'example.pdf', poppler_path=poppler_path)
+# images = convert_from_path(pdf_path = 'example.pdf', poppler_path=poppler_path) # Windows
+images = convert_from_path(pdf_path = 'example.pdf')  # Mac: brew install poppler.
  
 for i in range(len(images)):
       # Save pages as images in the pdf
